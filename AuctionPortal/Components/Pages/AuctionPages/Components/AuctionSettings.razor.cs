671fa4c74f00de647339d113afe49dbaab9ed16a
Ollie Lewington <olliedalpha@gmail.com>
2025-10-17 17:52:40 +0100

    Commiting initial UI changes before refactoring.

---
+++ AuctionPortal/Components/Pages/AuctionPages/Components/AuctionSettings.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using AuctionPortal.Data.Models;
+using System.Diagnostics;
+
+namespace AuctionPortal.Components.Pages.AuctionPages.Components;
+
+public partial class AuctionSettings : ComponentBase
+{
+    [Inject] NavigationManager Navigation { get; set; } = null!;
+
+    [Parameter] public Auction? Auction { get; set; } // Check for nullability and throw error if null
+
+    private string _auctionStatus = "Live";
+    private bool _notifyBidders = true;
+    private bool _notifyWinners = true;
+    private bool _notifyOutbid = false;
+
+    protected override void OnParametersSet()
+    {
+        Debug.WriteLine($"AuctionSetting received Auction: {Auction?.Id}");
+    }
+
+    private void DeleteAuction()
+    {
+        // Navigation logic to add product page
+    }
+
+    private void EditAuction()
+    {
+        // Navigation logic to add product page
+    }
+
+    private void CreateProduct()
+    {
+        // Navigation logic to add product page
+    }
+
+    private void SaveSettings()
+    {
+        // Logic to save auction settings
+    }
+}
